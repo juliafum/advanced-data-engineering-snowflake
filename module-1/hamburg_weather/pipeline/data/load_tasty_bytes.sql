435048d533139f789eb9dc9df435abf07268e2d0
juliafum <35400291+juliafum@users.noreply.github.com>
2025-11-14 22:02:30 +0100

    Change flag column type and add default value

--- module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
+++ module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
@@ -66,7 +66,7 @@ CREATE OR ALTER TABLE {{env}}_tasty_bytes.raw_pos.country
    city VARCHAR(16777216),
    city_population VARCHAR(16777216),
    city_id NUMBER(19,0),
-   flag NUMBER 2 not null
+   flag NUMBER(38,0) NOT NULL DEFAULT 2	
 );
 
 
